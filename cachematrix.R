c7f304e567f90832826766b987a8c1962ea02852
kcarycki <jerzybarszcz@macbook-jerzy-barszcz.local>
2014-08-22 13:00:58 +0200

    Edited comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,15 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## The functions are using lexicall scoping
+## for caching data for future use
+
 ## Write a short comment describing this function
 
-## Function creates list of functions to operate on matrix m
+## Function creates list of functions to operate on matrix x and matrix m
 ## which is an inverted matrix x. m is stored using operator <<-
-## in function makeCacheMatrix environment
+## in function makeCacheMatrix environment, it is in enclosing environment
+## of functions created by makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -24,9 +28,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## cacheSolve uses "matrix" created in makeCacheMatrix
-## actually it uses functions stored in object created by makeCacheMatrix
-## on data stored in makeCacheMatrix environment
+
+## cacheSolve uses "matrix" created in makeCacheMatrix.
+## Actually it uses functions stored in object created by makeCacheMatrix
+## on data stored in makeCacheMatrix environment.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
